6223be578c7cb313442f35a4117557d1bb0a07f5
e kaminski <trasski@hotmail.com>
2024-08-05 07:55:47 -0400

    add json encode/decode

--- src/player.py
+++ src/player.py
@@ -12,9 +12,50 @@ class Player():
         form = "Token {}: {} {} at {}"
         return form.format("P" + str(self.id) if self.id != None else "NPC",
                            self.name, self.token, self.location)
+    
+    def json_encode(self):
+        return {"name": self.name,
+                "id": self.id,
+                "token": self.token.json_encode(),
+                "decks": self.cards,
+                "location": self.location}
+    
+    # Note: this is a class function
+    def json_decode(json_dict):
+        if "Player" in json_dict:
+            name      = json_dict["Player"]["name"]
+            id        = json_dict["Player"]["id"]
+            token     = json_dict["Player"]["token"]
+            decks     = json_dict["Player"]["decks"]
+            location  = json_dict["Player"]["location"]
+            return Player(name=name,
+                          id=id,
+                          token=token,
+                          decks=decks,
+                          location=location
+                         )
+    
+    
+    
         
-
 if __name__ == '__main__':
+    import os
+    import json
+
+    class LocalEncoder(json.JSONEncoder):
+        def default(self, o):
+            if isinstance(o, Player):
+                return o.json_encode()
+            return json.JSONEncoder.default(self, o)
+    
+    class LocalDecoder(json.JSONDecoder):
+        def __init__(self, *args, **kwargs):
+            json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+        def object_hook(self, dct):
+            if 'Player' in dct:
+                return Player.json_decode(dct)
+            return dct
+
     p1   = Player("Fred",    token=Token("", "green"), location=0,    id=1)
     p2   = Player("Daphne",  token=Token("", "blue" ), location=23,   id=2)
     p3   = Player("Velma",   token=Token("", "red"  ), location=100,  id=3)
@@ -25,3 +66,12 @@ if __name__ == '__main__':
     players = [p1, p2, p3, p4, p5, npc1]
     for player in players:
         print(player)
+
+    filename = "temp/player.json"
+    with open(filename, 'w') as jsonfile:
+        json.dump(players, jsonfile, cls=LocalEncoder)
+    with open(filename, 'r') as jsonfile:
+        players_copy = json.load(jsonfile, cls=LocalDecoder)
+
+    assert len(players) == len(players_copy)
+    
--- src/token.py
+++ src/token.py
@@ -2,21 +2,62 @@ class Token():
     cShapes = ["circle", "triangle", "square", "pentagon", "hexagon",
                "star"]
     
-    def __init__(self, name, color=None, shape="circle", size=None, image=None):
+    def __init__(self, name, color=None, shape="circle", size=None,
+                 image_path=None, image=None):
         self.name = name
         self.color = color
         self.shape = shape
         self.size = size
+        self.image_path = image_path
         self.image = image
         
     def __str__(self):
         form = "Token: {} Shape={}-{}-{} Image={}"
         return form.format(self.name, self.color, self.shape, 
                            "auto" if self.size == None else self.size,
-                           self.image)
+                           self.image_path)
+    
+    def json_encode(self):
+        return {"name": self.name,
+                "color": self.color,
+                "shape": self.shape,
+                "size": self.size,
+                "image_path": self.image_path}
+    
+    # Note: this is a class function
+    def json_decode(json_dict):
+        if "Player" in json_dict:
+            name       = json_dict["Player"]["name"]
+            color      = json_dict["Player"]["color"]
+            shape      = json_dict["Player"]["shape"]
+            size       = json_dict["Player"]["size"]
+            image_path = json_dict["Player"]["image_path"]
+            return Player(name=name,
+                          color=color,
+                          shape=shape,
+                          size=int(size),
+                          image_path=image_path
+                         )
         
 
 if __name__ == '__main__':
+    import os
+    import json
+
+    class LocalEncoder(json.JSONEncoder):
+        def default(self, o):
+            if isinstance(o, Token):
+                return o.json_encode()
+            return json.JSONEncoder.default(self, o)
+    
+    class LocalDecoder(json.JSONDecoder):
+        def __init__(self, *args, **kwargs):
+            json.JSONDecoder.__init__(self, object_hook=self.object_hook, *args, **kwargs)
+        def object_hook(self, dct):
+            if 'Token' in dct:
+                return Token.json_decode(dct)
+            return dct
+        
     t1 = Token("G-er", "green", "square") 
     t2 = Token("B-er", "blue", "circle") 
     t3 = Token("R-er", "red", "triangle")  
@@ -26,3 +67,11 @@ if __name__ == '__main__':
     tokens = [t1, t2, t3, t4, t5]
     for token in tokens:
         print(token)
+
+    filename = "temp/token.json"
+    with open(filename, 'w') as jsonfile:
+        json.dump(tokens, jsonfile, cls=LocalEncoder)
+    with open(filename, 'r') as jsonfile:
+        tokens_copy = json.load(jsonfile, cls=LocalDecoder)
+
+    assert len(tokens) == len(tokens_copy)
