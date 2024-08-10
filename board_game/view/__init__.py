ac58903a8c484e8c686ba72d656bd0d8f4cac387
e kaminski <trasski@hotmail.com>
2024-08-09 22:49:55 -0400

    moved

---
+++ board_game/view/__init__.py
@@ -0,0 +1,13 @@
+import sys
+import os
+# sys.path.append('../view')                # so tests will find model
+# sys.path.append('./view')                 # so tests will find model
+# sys.path.append('./board_game')           # so model will find view
+# sys.path.append('./board_game/view')      # so model will find view
+
+# print()
+# print()
+# print("view.__init__", os.getcwd())
+# print("SysPath", sys.path)
+# print()
+# print()
