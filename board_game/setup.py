b462eee84ba460ed25a5b985429ba0277b80b0d2
e kaminski <trasski@hotmail.com>
2024-08-15 09:17:44 -0400

    cleanup

--- board_game/setup.py
+++ board_game/setup.py
@@ -3,7 +3,8 @@ print("BoardGame.board_game.setup")
 import os
 import sys
 
-# organization is package/module/submodule
-module = os.path.dirname(os.path.abspath(__file__))
-package = os.path.dirname(module)
-sys.path.insert(0, package)
+# organization is project/package/module/submodule
+# board_game is a package
+package = os.path.dirname(os.path.abspath(__file__))
+project = os.path.dirname(package)
+sys.path.insert(0, project)
