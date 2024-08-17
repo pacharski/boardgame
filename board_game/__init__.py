c22d12db7d458dc0e36db804c2dfc2f7cd281c8a
e kaminski <trasski@hotmail.com>
2024-08-17 19:06:38 -0400

    include all of the separate source files

--- board_game/__init__.py
+++ board_game/__init__.py
@@ -1,3 +1,26 @@
 from pathlib import Path
 print('Running' if __name__ == '__main__' else
       'Importing', Path(__file__).resolve())
+
+import os
+import sys
+here = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(here)
+
+from board_game.src.board import Board
+from board_game.src.point import Point
+from board_game.src.space import Space
+from board_game.src.connection import Connection
+from board_game.src.exit import Exit
+from board_game.src.marker import Marker
+from board_game.src.player import Player
+from board_game.src.card import Card, Deck
+from board_game.src.jsoninator import Jsoninator
+from board_game.src.hoard import Hoard, Treasure
+from board_game.src.horde import Horde, Creature
+from board_game.src.resizable import ViewPort, ResizableCanvas, ResizableImage
+from board_game.src.json_encoder import CompactJSONEncoder
+from board_game.src.marker_view import MarkerView
+from board_game.src.card_view import CardView
+from board_game.src.board_view import BoardView
+
