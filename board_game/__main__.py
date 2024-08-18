f12dd1e4194e8dfbcf06a7820f9f19f248813187
e kaminski <trasski@hotmail.com>
2024-08-17 23:00:59 -0400

    remove comment

--- board_game/__main__.py
+++ board_game/__main__.py
@@ -1,9 +1,6 @@
 # To run unit tests from boardgame directory, Run with:
 #     'python board_game'
 
-# organization is project/package/module/submodule
-# import setup
-
 from pathlib import Path
 print('Running' if __name__ == '__main__' else
       'Importing', Path(__file__).resolve())
