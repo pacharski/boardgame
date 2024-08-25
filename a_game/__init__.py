371f7d96114c72636574d2c563bfae087a327299
e kaminski <trasski@hotmail.com>
2024-08-25 17:53:27 -0400

    fix a_game player and test

--- a_game/__init__.py
+++ a_game/__init__.py
@@ -8,5 +8,6 @@ here = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(here)
 
 from a_game.src.a_game import Game
+from a_game.src.player import Player
 from a_game.src.game_view import GameView
 from a_game.src.table_view import TableView
