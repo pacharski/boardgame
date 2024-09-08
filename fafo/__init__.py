6252394047bf18c877644314fb007689034bf352
e kaminski <trasski@hotmail.com>
2024-09-08 09:39:35 -0400

    cleanup after refactor for card/deck

--- fafo/__init__.py
+++ fafo/__init__.py
@@ -5,10 +5,9 @@ sys.path.append(here)
 
 from fafo.src.game import GameAction, Game
 from fafo.src.player import Player
-from fafo.src.card import Card
+from fafo.src.card import Card, Deck
 from fafo.src.board import Board, Space
-# from fafo.src.space import Space
 from fafo.src.agent import Agent
 from fafo.src.game_view import GameView
 # from fafo.src.table_view import TableView
-# from fafo.src.qtrainer import QTrainer, Linear_QNet
+from fafo.src.qtrainer import QTrainer, Linear_QNet
