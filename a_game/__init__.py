5eed8c070b961c13a68fa6b5f0b7fd3219306e4d
e kaminski <trasski@hotmail.com>
2024-09-08 08:07:39 -0400

    move space to board.py

--- a_game/__init__.py
+++ a_game/__init__.py
@@ -8,6 +8,7 @@ here = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(here)
 
 from a_game.src.a_game import Game
+from a_game.src.board import Board, Space
 from a_game.src.player import Player
 from a_game.src.agent import Agent
 from a_game.src.game_view import GameView
